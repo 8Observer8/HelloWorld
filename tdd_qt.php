29d2501ef0d2d34e86ab2595ccc2788f96069772
Enzhaev Ivan <8observer8@gmail.com>
2014-01-22 17:00:48 +0400

    Подкорректировал текст в инструкции "Создание юнит-тестов (модульных тестов) на Qt C++ (TDD)"

--- tdd_qt.php
+++ tdd_qt.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Создание юнит-тестов на Qt C++ (TDD)</title>
+        <title>Создание юнит-тестов (модульных тестов) на Qt C++ (TDD).</title>
     </head>
     <body>
         <?php
@@ -17,48 +17,48 @@ and open the template in the editor.
 
         <h1>Создание юнит-тестов на Qt C++. Разработка ПО на Qt C++ по методике TDD (разработка ПО через тестирование)</h1>
 
-        <p>Принцип TDD (test-driven development) - сначала пишем тесты для пустого модуля (модуль - это функция или класс), а потом реализацию для этого модуля.</a></p>
+        <p>Принцип TDD (test-driven development) - сначала пишем тесты для пустого модуля (модуль - это функция или класс), а потом реализацию для этого модуля.</p>
 
-    <p>Такой способ разработки позволяет:</p>
+        <p>Такой способ разработки позволяет:</p>
 
-<li>описывать функциональность до написания самой функции</li>
-<li>демонстрировать, что функциональность реализована, как это и требовалось (заказчику, пользователю и\или самому себе)</li>
-<li>проводить рефакторинг (усовершенствовать код с целью оптимизации и/или для улучшения читабельности) без опасения нарушения функциональности</li>
+    <li>описывать функциональность до написания самой функции</li>
+    <li>демонстрировать, что функциональность реализована, как это и требовалось (заказчику, пользователю и\или самому себе)</li>
+    <li>проводить рефакторинг (усовершенствовать код с целью оптимизации и/или для улучшения читабельности) без опасения нарушения функциональности</li>
 
-<li>скачиваем и устанавливаем "Qt 5.2.0 for Windows 32-bit (MinGW 4.8, OpenGL, 689 MB)" <a href="http://qt-project.org/downloads">http://qt-project.org/downloads</a></li>
+    <li>скачиваем и устанавливаем "Qt 5.2.0 for Windows 32-bit (MinGW 4.8, OpenGL, 689 MB)" <a href="http://qt-project.org/downloads">http://qt-project.org/downloads</a></li>
 
-<li>запускаем Qt Creator</li>
+    <li>запускаем Qt Creator</li>
 
-<li>если у Вас Qt Creator на русском, то я рекомендовал бы переключиться на английский. Для этого перейдите в папку: C:\Qt\Qt5.2.0\Tools\QtCreator\share\qtcreator\translations и удалить все файлы в именах которых встречается "_ru"</li>
+    <li>если у Вас Qt Creator на русском, то я рекомендовал бы переключиться на английский. Для этого перейдите в папку: C:\Qt\Qt5.2.0\Tools\QtCreator\share\qtcreator\translations и удалить все файлы в именах которых встречается "_ru"</li>
 
-<p>Для примера разработаем программу, которая решает задачу: <a href="http://acmp.ru/index.asp?main=task&id_task=25">http://acmp.ru/index.asp?main=task&id_task=25</a></p>
+    <p>Для примера разработаем программу, которая решает задачу: <a href="http://acmp.ru/index.asp?main=task&id_task=25">http://acmp.ru/index.asp?main=task&id_task=25</a></p>
 
-<li>в меню Qt Creator'а выбираем "File" -> "New File or Project..."</li>
+    <li>в меню Qt Creator'а выбираем "File" -> "New File or Project..."</li>
 
-<li>в разделе "Projects" выбираем "Other Project" -> во второй колонке выбираем "Qt Unit Test"</li>
+    <li>в разделе "Projects" выбираем "Other Project" -> во второй колонке выбираем "Qt Unit Test"</li>
 
-<li>нажимаем кнопку "Choose"</li>
+    <li>нажимаем кнопку "Choose"</li>
 
-<li>в поле "Name" вводим: MyFirstUnit</li>
+    <li>в поле "Name" вводим: MyFirstUnit</li>
 
-<li>нажимаем "Next" -> "Next" -> "Next" -> "Finish"</li>
+    <li>нажимаем "Next" -> "Next" -> "Next" -> "Finish"</li>
 
-<li>нажимаем правой кнопкой мыши по проекту в разделе "Projects" -> нажимаем "Add New...", как показано на рисунке:</li><br />
+    <li>нажимаем правой кнопкой мыши по проекту в разделе "Projects" -> нажимаем "Add New...", как показано на рисунке:</li><br />
 
-<img src="img/tdd_qt_imgs/001.png" />
+    <img src="img/tdd_qt_imgs/001.png" />
 
-<li>в разделе "Files and Clases" выбираем "C++" -> во втором разделе выбираем "C++ Class"</li>
+    <li>в разделе "Files and Clases" выбираем "C++" -> во втором разделе выбираем "C++ Class"</li>
 
-<li>нажимаем кнопку "Choose"</li>
+    <li>нажимаем кнопку "Choose"</li>
 
-<li>в поле "Class name" вводим: MyClass</li>
+    <li>в поле "Class name" вводим: MyClass</li>
 
-<li>нажимаем кнопку "Next" -> нажимаем кнопку "Finish"</li>
+    <li>нажимаем кнопку "Next" -> нажимаем кнопку "Finish"</li>
 
-<li>открываем файл "myclass.h" и меняем его содержимое на следующее:</li>
+    <li>открываем файл "myclass.h" и меняем его содержимое на следующее:</li>
 
-<p><strong>myclass.h</strong></p>
-<pre>
+    <p><strong>myclass.h</strong></p>
+    <pre>
 <code class="language-cpp">
 #ifndef MYCLASS_H
 #define MYCLASS_H
@@ -71,16 +71,16 @@ public:
 
 #endif // MYCLASS_H
 </code>
-</pre>
+    </pre>
 
-<li>нажимаем правой кнопкой по функции compare() -> выбираем "Refactor" -> выбираем "Add Definition in myclass.cpp", как показано на рисунке:</li>
+    <li>нажимаем правой кнопкой по функции compare() -> выбираем "Refactor" -> выбираем "Add Definition in myclass.cpp", как показано на рисунке:</li>
 
-<img src="img/tdd_qt_imgs/002.png" />
+    <img src="img/tdd_qt_imgs/002.png" />
 
-<li>открываем файл "myclass.cpp" и меняем его содержимое на следующее:</li>
+    <li>открываем файл "myclass.cpp" и меняем его содержимое на следующее:</li>
 
-<p><strong>myclass.cpp</strong></p>
-<pre>
+    <p><strong>myclass.cpp</strong></p>
+    <pre>
 <code class="language-cpp">
 #include "myclass.h"
 
@@ -89,26 +89,26 @@ int MyClass::compare(int first, int second)
     return 0;
 }
 </code>
-</pre>
+    </pre>
 
-<li>открываем файл "tst_myfirstunittest.cpp"</li>
+    <li>открываем файл "tst_myfirstunittest.cpp"</li>
 
-<p>Файле "tst_myfirstunittest.cpp" нам нужно в класс "MyFirstUnitTest" добавить функцию для инициализации данных testCase1_data() (данные - это входные данные и ожидаемый результат)</p>
+    <p>Файле "tst_myfirstunittest.cpp" нам нужно в класс "MyFirstUnitTest" добавить функцию для инициализации данных testCase1_data() (данные - это входные данные и ожидаемый результат)</p>
 
-<p>Ещё нам нужно написать реализацию для функций testCase1_data() и testCase1()</p>
+    <p>Ещё нам нужно написать реализацию для функций testCase1_data() и testCase1()</p>
 
-<p>testCase1_data() - инициализирует данные (входные данные и ожидаемый результат)</p>
+    <p>testCase1_data() - инициализирует данные (входные данные и ожидаемый результат)</p>
 
-<p>testCase1() - запускает тесты, то есть производит вызовы тестируемой функции и сравнивает каждый результат вызова с соответствующим ожидаемым результатом</p>
+    <p>testCase1() - запускает тесты, то есть производит вызовы тестируемой функции и сравнивает каждый результат вызова с соответствующим ожидаемым результатом</p>
 
-<p>По условию задачи, нужно написать функцию, которая возращает: -1 - если первый аргумент меньше второе, 1 - если первый аргумент больше второе и 0 - если оба аргумента равны друг другу</p>
+    <p>По условию задачи, нужно написать функцию, которая возвращает: -1 - если первый аргумент меньше второе, 1 - если первый аргумент больше второе и 0 - если оба аргумента равны друг другу</p>
 
-<p>Напишем 10 тестов. В данном случае мы могли бы обойтись и меньшем количеством</p>
+    <p>Напишем 10 тестов. В данном случае мы могли бы обойтись и меньшем количеством</p>
 
-<li>меняем содержимое файла "tst_myfirstunittest.cpp" на следующее:</li>
+    <li>меняем содержимое файла "tst_myfirstunittest.cpp" на следующее:</li>
 
-<p><strong>tst_myfirstunittest.cpp</strong></p>
-<pre>
+    <p><strong>tst_myfirstunittest.cpp</strong></p>
+    <pre>
 <code class="language-cpp">
 #include &lt;QString&gt;
 #include &lt;QtTest&gt;
@@ -164,22 +164,22 @@ QTEST_APPLESS_MAIN(MyFirstUnitTest)
 
 #include "tst_myfirstunittest.moc"
 </code>
-</pre>
+    </pre>
 
-<li>нажимаем Ctrl+Shift+S, чтобы сохранить всё</li>
+    <li>нажимаем Ctrl+Shift+S, чтобы сохранить всё</li>
 
-<li>нажимаем Ctrl+R для построения и запуска программы</li>
+    <li>нажимаем Ctrl+R для построения и запуска программы</li>
 
-<li>в окне "Application Output" мы можем видеть, что удачно завершился только первый тест, так как фукнция compare() всегда возвращает ноль. Мы передали два нуля и ожидали ноль в первом тесте.
+    <li>в окне "Application Output" мы можем видеть, что удачно завершился только первый тест, так как функция compare() всегда возвращает ноль. Мы передали два нуля и ожидали ноль в первом тесте.
 
-    <img src="img/tdd_qt_imgs/003.png" />
+        <img src="img/tdd_qt_imgs/003.png" />
 
-    <p>Пишем функционал compare() в файле "myclass.cpp", чтобы она делала, то что мы ожидаем. С помощью тестов мы описали, что мы ожидаем от работы функции compare(). Если мы реализуем неправильно, то тесты нам это покажут.</p>
+        <p>Пишем функционал compare() в файле "myclass.cpp", чтобы она делала, то что мы ожидаем. С помощью тестов мы описали, что мы ожидаем от работы функции compare(). Если мы реализуем неправильно, то тесты нам это покажут.</p>
 
-<li>меняем содержимое файла "myclass.cpp" на следующее:</li>
+    <li>меняем содержимое файла "myclass.cpp" на следующее:</li>
 
-<p><strong>myclass.cpp</strong></p>
-<pre>
+    <p><strong>myclass.cpp</strong></p>
+    <pre>
 <code class="language-cpp">
 #include "myclass.h"
 
@@ -198,21 +198,21 @@ int MyClass::compare(int first, int second)
     return result;
 }
 </code>
-</pre>
+    </pre>
 
-<li>нажимаем Ctrl+R для построения и запуска программы</li>
+    <li>нажимаем Ctrl+R для построения и запуска программы</li>
 
-<p>Теперь мы видим, что тесты пройдены успешно:</p>
+    <p>Теперь мы видим, что тесты пройдены успешно:</p>
 
-<img src="img/tdd_qt_imgs/004.png" />
+    <img src="img/tdd_qt_imgs/004.png" />
 
-<p><strong>Примечание:</strong> Чтобы отформатировать код (выровнить), нужно нажать Ctrl+A (чтобы выделить), потом нажать Ctrl+I.</p>
+    <p><strong>Примечание:</strong> Чтобы отформатировать код (выровнить), нужно нажать Ctrl+A (чтобы выделить), потом нажать Ctrl+I.</p>
 
-<p>Подробнее о TDD можно почитать здесь: <a href="ru.wikipedia.org/wiki/Разработка_через_тестирование">ru.wikipedia.org/wiki/Разработка_через_тестирование</a></p>
-<p>Обратная связь: 8observer8@gmail.com</p>
-<!--<p>Вопросы и комментарии пишите в моей теме: <a href="http://programmersforum.ru/showthread.php?t=225824">http://programmersforum.ru/showthread.php?t=225824</a></p>-->
-<p>Материальная благодарность:</p>
-<p>Яндекс Деньги: 410011677695050</p>
-<p>WebMoney (WMID): 416161350356</p>
+    <p>Подробнее о TDD можно почитать здесь: <a href="ru.wikipedia.org/wiki/Разработка_через_тестирование">ru.wikipedia.org/wiki/Разработка_через_тестирование</a></p>
+    <p>Обратная связь: 8observer8@gmail.com</p>
+    <!--<p>Вопросы и комментарии пишите в моей теме: <a href="http://programmersforum.ru/showthread.php?t=225824">http://programmersforum.ru/showthread.php?t=225824</a></p>-->
+    <p>Материальная благодарность:</p>
+    <p>Яндекс Деньги: 410011677695050</p>
+    <p>WebMoney (WMID): 416161350356</p>
 </body>
 </html>
